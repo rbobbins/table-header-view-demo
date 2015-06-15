0c2bd1eb55618814683e954fa3b710361fbff1e8
Rachel Bobbins <rbobbins@stitchfix.com>
2015-06-15 09:54:56 -0700

    Demonstrates problem with table header view

--- TableHeaderViewDemo/ViewController.swift
+++ TableHeaderViewDemo/ViewController.swift
@@ -8,16 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTableHeader()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    //MARK: UITableViewDataSource
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellIdentifier") as! UITableViewCell
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    //MARK: Table Header
+
+    func setupTableHeader() {
+        let headerView = UIView()
+        headerView.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+        let headerLabel = UILabel()
+        headerLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        headerLabel.text = "This is a tableHeaderView"
+        headerView.addSubview(headerLabel)
+
+
+        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-(0)-[label]-(0)-|", options: nil, metrics: nil, views: ["label": headerLabel])
+
+        let hConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-(0)-[label]-(0)-|", options: nil, metrics: nil, views: ["label": headerLabel])
+
+        headerView.addConstraints(vConstraints)
+        headerView.addConstraints(hConstraints)
+
+        tableView.tableHeaderView = headerView
+        tableView.tableHeaderView?.setTranslatesAutoresizingMaskIntoConstraints(false)
     }
 
 
